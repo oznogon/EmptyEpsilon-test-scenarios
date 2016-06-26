d744b6689610a75e5c5caa2fd9d4d9b45c608956
oznogon <garrett+ozgh@oznogon.com>
2016-06-26 11:06:28 -0700

    Init.

---
+++ scenario_99_turrettest.lua
@@ -0,0 +1,8 @@
+-- Name: Turret test
+-- Description: Testing turrets
+-- Type: Basic
+
+PlayerSpaceship():setTemplate("Atlantis"):setPosition(48, 23)
+CpuShip():setFaction("Kraylor"):setTemplate("Tug"):setCallSign("SS2"):setPosition(-256, 267)
+CpuShip():setFaction("Kraylor"):setTemplate("Tug"):setCallSign("SS3"):setPosition(367, 278)
+CpuShip():setFaction("Kraylor"):setTemplate("Tug"):setCallSign("VS4"):setPosition(411, -267)
